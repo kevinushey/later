6d9eb00db8bdbda16d9112c1725156b4bf2f5693
Kevin Ushey <kevinushey@gmail.com>
2015-10-31 20:31:13 -0700

    slightly tidier

--- R/handlers.R
+++ R/handlers.R
@@ -1,9 +1,6 @@
-## Handlers used for 'defer' calls.
-## Attached as a list of expressions for
-## the 'handlers' attribute on the
-## environment, with 'on.exit' called to
-## ensure those handlers get executed on
-## exit.
+## Handlers used for 'defer' calls. Attached as a list of expressions for the
+## 'handlers' attribute on the environment, with 'on.exit' called to ensure
+## those handlers get executed on exit.
 
 get_handlers <- function(envir) {
   result <- attr(envir, "handlers")
@@ -15,11 +12,7 @@ set_handlers <- function(envir, handlers) {
   if (is.null(attr(envir, "has_handlers"))) {
 
     attr(envir, "has_handlers") <- TRUE
-
-    call <- substitute(
-      f(x),
-      list(f = execute_handlers, x = envir)
-    )
+    call <- make_call(execute_handlers, envir)
 
     do.call(
       base::on.exit,
@@ -29,17 +22,10 @@ set_handlers <- function(envir, handlers) {
   }
 }
 
-execute <- function(object) {
-  if (is.language(object))
-    eval(object)
-  else if (is.function(object))
-    object()
-}
-
 execute_handlers <- function(envir) {
   handlers <- get_handlers(envir)
   for (handler in handlers)
-    execute(handler)
+    eval(handler)
 }
 
 add_handler <- function(envir, handler, front) {
