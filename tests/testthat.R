5ddbf26288d208729e0787c024efa480d79a7a44
Kevin Ushey <kevinushey@gmail.com>
2016-05-13 20:54:00 -0500

    conditional tests

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,4 +1,7 @@
-library(testthat)
-library(later)
-
-test_check("later")
+if (requireNamespace("testthat", quietly = TRUE)) {
+  library(testthat)
+  library(later)
+  test_check("later")
+} else {
+  warning("'testthat' is not available -- tests will not be run")
+}
