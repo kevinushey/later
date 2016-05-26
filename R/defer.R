ebea46a22c23139d737dedf1b307e4776e1c05ce
Kevin Ushey <kevinushey@gmail.com>
2016-05-25 21:44:30 -0700

    move 'handlers.R' to 'defer.R'

--- R/defer.R
+++ R/defer.R
@@ -72,3 +72,38 @@ defer_parent <- function(expr, priority = c("first", "last")) {
     list(expr = substitute(expr), envir = parent.frame(2), priority = priority)
   ), envir = parent.frame())
 }
+
+
+## Handlers used for 'defer' calls. Attached as a list of expressions for the
+## 'handlers' attribute on the environment, with 'on.exit' called to ensure
+## those handlers get executed on exit.
+
+get_handlers <- function(envir) {
+  result <- attr(envir, "handlers")
+  if (!is.list(result)) list() else result
+}
+
+set_handlers <- function(envir, handlers) {
+  attr(envir, "handlers") <- handlers
+  if (is.null(attr(envir, "has_handlers"))) {
+    attr(envir, "has_handlers") <- TRUE
+    call <- make_call(execute_handlers, envir)
+    do.call(base::on.exit, list(substitute(call), TRUE), envir = envir)
+  }
+}
+
+execute_handlers <- function(envir) {
+  handlers <- get_handlers(envir)
+  for (handler in handlers)
+    eval(handler)
+}
+
+add_handler <- function(envir, handler, front) {
+  handlers <- if (front)
+    c(handler, get_handlers(envir))
+  else
+    c(get_handlers(envir), handler)
+
+  set_handlers(envir, handlers)
+  handler
+}
